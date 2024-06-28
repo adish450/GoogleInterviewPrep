9ecdf402b6c0adf288d2a80b78bb28ea6a873d72
adish17 <adishirfan17@gmail.com>
2024-06-28 07:19:32 +0530

    third code submission using array of lists

--- 347. Top K Frequent Elements/src/Solution.java
+++ 347. Top K Frequent Elements/src/Solution.java
@@ -1,77 +1,46 @@
 class Solution {
-
-    private class Pair {
-        int freq;
-        int num;
-
-        Pair(int freq, int num) {
-            this.freq = freq;
-            this.num = num;
-        }
-    }
-
     public int[] topKFrequent(int[] nums, int k) {
 
-        // freq vs number map
-        HashMap<Integer, Integer> elementMap = new HashMap<>();
-
-        for (int i = 0; i < nums.length; i++) {
+        ArrayList<Integer> [] list = new ArrayList[nums.length + 1];
+        //freq map
+        HashMap<Integer, Integer> freqMap = new HashMap<Integer, Integer>();
+        for (int i = 0 ; i < nums.length; i++) {
             int num = nums[i];
-            // check if the element already exists
-            if (elementMap.get(num) != null) {
-                Integer freq = elementMap.get(num);
-                freq++;
-                elementMap.put(num, freq);
+            if (freqMap.containsKey(num)) {
+                freqMap.put(num, freqMap.get(num) + 1);
             } else {
-                // add element
-                elementMap.put(num, 1);
+                freqMap.put(num, 1);
             }
         }
 
-        int ans[] = new int[k];
-        int count = 0;
-        if (elementMap.size() <= k) {
-        	// if size is less than k, it means we can take all the elements of the map as it is, as all will be part of the answer.
-            for (Integer key : elementMap.keySet()) {
-                ans[count++] = key;
+        // build array of list with indexing of frequency
+        for (Integer key : freqMap.keySet()) {
+            int freq = freqMap.get(key);
+            if (list[freq] == null) {
+                ArrayList<Integer> nl = new ArrayList<Integer>();
+                nl.add(key);
+                list[freq] = nl; 
+            } else {
+                list[freq].add(key);
             }
-        } else {
-            // create a min heap
-            PriorityQueue<Pair> pq = new PriorityQueue<Pair>(new Comparator<>() {
-                @Override
-                public int compare(Pair p1, Pair p2) {
-                    if (p1.freq - p2.freq < 0) {
-                        return -1;
-                    } else if (p1.freq - p2.freq > 0) {
-                        return 1;
-                    } else {
-                        if (p1.num - p2.num < 0) {
-                            return -1;
-                        } else {
-                            return 1;
-                        }
+        }
+
+        int count = 0;
+        int[] ans = new int[k];
+        for (int i = list.length - 1 ; i >= 0 ; i--) { // traverse from last as indexing is the answer
+            if (list[i] != null) {
+                for (Integer i1 : list[i]) {
+                    ans[count++] = (int)i1;
+                    if (count == k) {
+                        break;
                     }
                 }
-            });
-
-            for (Integer key : elementMap.keySet()) {
-                Pair np = new Pair(elementMap.get(key), key);
-                pq.add(np);
-                if (pq.size() > k) {
-                	// this solution only works because we are using a min heap which has exactly k elements in it,
-                    //when we traverse the map and add a new element,
-                    //if the size exceeds k the min element is no longer needed and we remove it.
-                    pq.poll(); 
-                }
             }
-
-            for (int i = 0; i < k; i++) {
-                Pair rp = pq.poll();
-                ans[i] = rp.num;
-            }
+            if (count == k) {
+                break;
+            } 
         }
 
         return ans;
-
     }
 }
